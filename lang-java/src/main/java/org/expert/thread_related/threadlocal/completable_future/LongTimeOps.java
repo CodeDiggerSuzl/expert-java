76512cdf6b5fb1806726c6f8932579b28b8a0e1b
suzailong <suzailong@didachuxing.com>
2023-09-09 18:12:12 +0800

    feat:completable_future !

---
+++ lang-java/src/main/java/org/expert/thread_related/threadlocal/completable_future/LongTimeOps.java
@@ -0,0 +1,8 @@
+package org.expert.thread_related.threadlocal.completable_future;
+
+public class LongTimeOps {
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
